b2aa499d2b876679cf07d7cb6a0203aeab01cfd9
mmeiler <maxmeiler@proton.me>
2024-11-16 07:17:24 +0100

    feat: added initialization function to gather input from pipe and getting and instance of the B15F. Put it into own function because of testing without a B15F Board

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::io::BufRead;
+use b15r::B15F;
+
+fn read_pipe() -> String {
+    let stdin = std::io::stdin(); 
+    let handle = stdin.lock();
+
+    for line in handle.lines() {
+        match line {
+            Ok(line) => return line,
+            Err(e) => {
+                eprintln!("Error reading line: {}", e);
+                break;
+            }
+        }
+    }
+    return "".to_string();
+}
+
+fn b15f_init() -> (B15F, String) {
+    (B15F::get_instance(), read_pipe())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let (drv, pip) = b15f_init();
 }
